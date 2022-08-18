33892661f2194e6cf710dee59199ffe1cab0cfc4
thomas IMPERY <thomas.impery@gmail.com>
2022-08-18 09:46:20 +0200

    ✨ Survey - Parse content from firestore

--- lib/survey/survey.dart
+++ lib/survey/survey.dart
@@ -1,23 +1,44 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:demos/survey/decision.dart';
 import 'package:demos/survey/survey_body.dart';
+import 'package:demos/survey/survey_data.dart';
 import 'package:flutter/material.dart';
 import 'package:sliding_up_panel/sliding_up_panel.dart';
 
 import 'additionnal_info.dart';
 
-class Survey extends StatelessWidget {
-  Survey({Key? key}) : super(key: key) {
+class Survey extends StatefulWidget {
+  const Survey({Key? key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => _SurveyState();
+}
+
+class _SurveyState extends State<Survey> {
+  late List<SurveyData> _datas;
+
+  _SurveyState() {
     getData();
   }
 
   Future<void> getData() async {
     var db = FirebaseFirestore.instance;
+    _datas = <SurveyData>[];
 
     db.collection("surveys").get().then((event) {
       for (var doc in event.docs) {
         print("${doc.id} => ${doc.data()}");
+
+        var data = doc.data();
+        var survey = SurveyData(
+            question: data['question'],
+            addionnalInfo: data['additionnal_info'],
+            decisions: List<String>.from(data["decision"]));
+
+        _datas.add(survey);
       }
+
+      print("");
     });
   }
 
---
+++ lib/survey/survey_data.dart
@@ -0,0 +1,10 @@
+class SurveyData {
+  String question;
+  String addionnalInfo;
+  List<String> decisions;
+
+  SurveyData(
+      {required this.question,
+      required this.addionnalInfo,
+      required this.decisions});
+}
